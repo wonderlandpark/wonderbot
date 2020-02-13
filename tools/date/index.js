47f8e130e240cebeeb5557a6cdb1db169fb83bd8
원더 <wonderbotsupport@naver.com>
2020-02-13 16:33:39 +0900

    Updating Timezone for server

--- tools/date/index.js
+++ tools/date/index.js
@@ -1,16 +1,20 @@
 const moment = require("moment");
 require("moment-with-locales-es6");
-
+require('moment-timezone');
 Date.prototype.format = function(locale) {
+  moment().tz('Asia/Seoul');
   moment.locale(locale);
-  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").format("llll");
+  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").tz('Asia/Seoul')
+  .format("llll");
 };
 Date.prototype.fromNow = function(locale) {
   moment.locale(locale);
-  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").fromNow();
+  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").tz('Asia/Seoul')
+  .fromNow();
 };
 
 Date.prototype.textFormat = function(format, locale) {
   moment.locale(locale);
-  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").format(format);
+  return moment(this, "YYYY-MM-DDTHH:mm:ssZ").tz('Asia/Seoul')
+  .format(format);
 };
