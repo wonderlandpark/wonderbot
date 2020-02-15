5fbf97b6b695fb3a660764b2735cfe3c5d6bbbd4
원더 <wonderbotsupport@naver.com>
2020-02-15 12:34:59 +0900

    Created Notice Command

--- commands/dev/index.js
+++ commands/dev/index.js
@@ -3,3 +3,4 @@ module.exports.reboot = require("./reboot");
 module.exports.wonderbot = require("./wonderbot");
 module.exports.botinvite = require("./botinvite");
 module.exports.errortest = require("./errortest");
+module.exports.notice = require("./notice");
