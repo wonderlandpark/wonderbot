ea45c1f36c8115fb6026bfda34106ca201a13685
원더 <wonderbotsupport@naver.com>
2020-02-23 23:37:10 +0900

    Update corona.js

--- commands/season/corona.js
+++ commands/season/corona.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 module.exports.execute = async (client, message, locale, embed) => {
+  message.reply('해당 기능은 점검중입니다.')
   const page = await fetch(
     'https://namu.wiki/w/%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D-19/%EA%B2%BD%EA%B3%BC/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%20%ED%99%95%EC%A7%84%EC%9E%90%20%ED%98%84%ED%99%A9'
   ).then(b => b.text());
@@ -53,7 +54,7 @@ module.exports.execute = async (client, message, locale, embed) => {
 };
 module.exports.props = {
   name: 'corona',
-  perms: 'dev',
+  perms: 'general',
   alias: ['코로나'],
   args: []
 };
