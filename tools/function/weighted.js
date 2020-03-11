ca94e75514dfff9bec4192bea9b089267511543e
원더 <wonderbotsupport@naver.com>
2020-03-11 11:57:53 +0900

    Fixed Bug
    At https://github.com/wonderlandpark/wonderbot/issues/16

--- tools/function/weighted.js
+++ tools/function/weighted.js
@@ -4,6 +4,9 @@ module.exports = function(spec) {
   // eslint-disable-next-line guard-for-in
   for (var i in spec) {
     sum += spec[i];
-    if (r <= sum) return i;
+    if (r <= sum) {
+      if(undefined) return spec[Object.keys(spec)[0]];
+      else return i;
+    }
   }
 };
