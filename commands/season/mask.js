20d6761f27103f9ec0dec87f74749ed2a409e7c3
원더 <wonderbotsupport@naver.com>
2020-03-11 13:38:49 +0900

    Fixed Bug
    Fixed https://github.com/wonderlandpark/wonderbot/issues/21

--- commands/season/mask.js
+++ commands/season/mask.js
@@ -10,7 +10,7 @@ module.exports.execute = async (client, message, locale, embed) => {
   r = list.search(message.data.args)[0]
   if(status.count == 0) return m.edit(`> ❌ 검색결과가 없습니다 \n\`${r ? r.element : '검색결과 없음'}\`을/를 찾으셨나요?\n💡 TIP: **구** 이상의 정확한 주소를 입력해주세요!!`)
   for(i=0; i<10; i++){
-    if(status.stores[i]) embed.addField(status.stores[i].name, `주소: \`${status.stores[i].addr}\`\n${statusCode[status.stores[i]['remain_stat']]}`)
+    if(status.stores[i]) embed.addField(status.stores[i].name, `주소: \`${status.stores[i].addr}\`\n${statusCode[status.stores[i]['remain_stat']] || '❔ 알 수 없음'}`)
   }
   m.edit({content: '> ✅ 마스크 정보를 불러왔습니다. 잘못된 정보는 팀이 책임지지 않습니다.', embed})
 };
