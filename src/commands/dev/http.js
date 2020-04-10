ed6ae421c082869abaa12a6a42f71aec6b26a3dc
원더 <wonderbotsupport@naver.com>
2020-04-10 16:21:31 +0900

    Added Http Status Code command

---
+++ src/commands/dev/http.js
@@ -0,0 +1,26 @@
+const codes = require('./http-status-code.json').list
+module.exports.execute = async (
+  client,
+  message,
+  locale,
+  embed,
+  tools,
+  knex,
+  props,
+  data
+) => {
+    const res = codes.find(r=> r.code.startsWith(message.data.arg[0])|| r.desc.toLowerCase().includes(message.data.args.toLowerCase()))
+    if(!res) return message.reply('`NORES`')
+    else {
+      embed.setTitle(res.code)
+      embed.setDescription(res.desc)
+      message.reply(embed)
+    }
+  }
+
+module.exports.props = {
+  name: 'http',
+  perms: 'general',
+  alias: ['statuscode'],
+  args: []
+}
