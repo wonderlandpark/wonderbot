e63075409b8660f81e4b0fca0996cedc8d7cfad0
원더 <wonderbotsupport@naver.com>
2020-04-13 21:27:58 +0900

    Serverinfo

--- src/commands/info/serverinfo.js
+++ src/commands/info/serverinfo.js
@@ -19,6 +19,9 @@ module.exports.execute = async (
    embed.addField(locale.commands.serverinfo.channel, locale.commands.serverinfo.channelDesc.bind({ text: message.guild.channels.cache.filter(r=> r.type === 'text').size, category: message.guild.channels.cache.filter(r=> r.type === 'category').size, voice: message.guild.channels.cache.filter(r=> r.type === 'voice').size}), true)
    embed.addField(locale.commands.serverinfo.verification, locale.commands.serverinfo.verificationLevel[message.guild.verificationLevel]) 
    embed.addField(locale.commands.serverinfo.filterName, locale.commands.serverinfo.filter[message.guild.explicitContentFilter])
+   embed.addField(locale.commands.serverinfo.roles, message.guild.roles.cache.array().join(', ').length > 1000 ?  message.guild.roles.cache.array().splice(0, 42).join(', ')  + locale.commands.serverinfo.more.bind({count: message.guild.roles.cache.array().length - 42}) : message.guild.roles.cache.array().join(', '))
+   embed.addField(locale.commands.serverinfo.channels, message.guild.channels.cache.array().join(', ').length > 1000 ?  message.guild.channels.cache.array().splice(0, 42).join(', ')  + locale.commands.serverinfo.more.bind({count: message.guild.channels.cache.array().length - 42}) : message.guild.channels.cache.array().join(', '))
+   embed.addField(locale.commands.serverinfo.emojis, message.guild.emojis.cache.array().join(', ').length > 1000 ?  message.guild.emojis.cache.array().splice(0, 19).join(', ')  + locale.commands.serverinfo.more.bind({count: message.guild.roles.cache.array().length - 19}) : message.guild.emojis.cache.array().join(', '))
    if(message.guild.iconURL()) embed.setThumbnail(message.guild.iconURL()) 
 
    else {
