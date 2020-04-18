d8cf3d7e7025401f3e7e552a89201765fd6a3a5c
원더 <wonderbotsupport@naver.com>
2020-04-18 09:54:39 +0900

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `users` ( `id` TEXT NOT NULL  , `join` INT NOT NULL , `money` INT NOT NULL DEFAULT '0' , `money_cooldown` INT NOT NULL DEFAULT '0' , `badges` TEXT NOT NULL DEFAULT '[]', `items` TEXT NOT NULL DEFAULT "{}", `multiples` INT NOT NULL DEAFULT 0, `language` TEXT NOT NULL DEFAULT "ko", `cooldown` TEXT NOT NULL DEFAULT "{}", `action` BOOLEAN NOT NULL DEFAULT false, `mail` TEXT NOT NULL DEFAULT "[]");
+CREATE TABLE `users` ( `id` TEXT NOT NULL  , `join` INT NOT NULL , `money` INT NOT NULL DEFAULT '0' , `money_cooldown` INT NOT NULL DEFAULT '0' , `badges` TEXT NOT NULL DEFAULT '[]', `items` TEXT NOT NULL DEFAULT "{}", `multiples` INT NOT NULL DEAFULT 0, `language` TEXT NOT NULL DEFAULT "ko", `cooldown` TEXT NOT NULL DEFAULT "{}", `action` BOOLEAN NOT NULL DEFAULT false, `mails` TEXT NOT NULL DEFAULT "[]");
 CREATE TABLE `guilds` (`id` TEXT NOT NULL);
 CREATE TABLE `items` ( `name` TEXT NOT NULL, `last_change` INT, `money` INT NOT NULL DEFAULT '0' , `change` INT NOT NULL DEFAULT '0');
 CREATE TABLE `blacklist` (`id` TEXT NOT NULL, `time` INT NOT NULL, `why` TEXT NOT NULL DEFAULT 'none');
