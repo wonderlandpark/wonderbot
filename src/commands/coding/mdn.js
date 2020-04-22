4ce29cb822708bd7dbc3e07ca6732034d09eb95a
원더 <wonderbotsupport@naver.com>
2020-04-22 21:59:56 +0900

    Update mdn.js

--- src/commands/coding/mdn.js
+++ src/commands/coding/mdn.js
@@ -5,11 +5,16 @@ module.exports.execute =  async (client, message, locale, embed) => {
     if(!message.data.args) return message.reply(locale.error.usage(message.data.cmd))
     const queryString = message.data.args
     const body = await fetch(`https://mdn.pleb.xyz/search?q=${encodeURI(queryString)}`).then(res=>res.json())
+    let main = body.Translations.find(r=> r.Locale === 'ko')
+    if(!main) { 
+        main = body
+        main.noTran = true
+    }
     if (!body.URL || !body.Title || !body.Summary) {
         return message.reply('없어! 없어! 없다고!')
     }
 
-    const main = body.Translations.find(r=> r.Locale === 'ko')
+
     const turndown = new Turndown()
     embed
         .setAuthor('MDN', 'https://i.imgur.com/DFGXabG.png', 'https://developer.mozilla.org/')
@@ -17,7 +22,7 @@ module.exports.execute =  async (client, message, locale, embed) => {
         .setTitle(main.Title)
         .setDescription(turndown.turndown(main.Summary.replace(/<code><strong>(.+)<\/strong><\/code>/g, '<strong><code>$1</code></strong>')))
 			
-    return message.channel.send(embed)
+    return message.channel.send(main.noTran ? { content: '> 이 페이지는 한글 번역이 완료되지 않았습니다.\n> 영어로 출력합니다.', embed} : embed )
 }
 
 
