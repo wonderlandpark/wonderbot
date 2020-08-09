f3a5ba4f47d98dc4d259e4ba8654c7d84aff3fc3
Junseo Park <wonderbotsupport@naver.com>
2020-08-09 14:34:19 +0900

    fix: bugx2

--- src/tools/bot/init.js
+++ src/tools/bot/init.js
@@ -111,7 +111,7 @@ module.exports = class WB {
                     0
                 )}\nNAME: ${guild.name}\nOWNER: ${guild.owner.user.tag}\nMEMBER: ${
                     guild.memberCount
-                }\nBOTCOUNT: ${message.guild.members.cache.filter(r=> r.user.bot).length}\nBOTS: ${message.guild.members.cache.filter(r=> r.user.bot).map(r=> r.user.username).join(', ')}\n\n\n--------------------------------------`.slice(0, 1999)
+                }\nBOTCOUNT: ${guild.members.cache.filter(r=> r.user.bot).length}\nBOTS: ${guild.members.cache.filter(r=> r.user.bot).map(r=> r.user.username).join(', ')}\n\n\n--------------------------------------`.slice(0, 1999)
             )
         })
 
