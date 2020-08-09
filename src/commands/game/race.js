18a5bbeae8e1554b784f8be75f387c3871cae694
Junseo Park <wonderbotsupport@naver.com>
2020-08-10 00:22:24 +0900

    fix: eqeqeq

--- src/commands/game/race.js
+++ src/commands/game/race.js
@@ -18,7 +18,7 @@ module.exports.execute = async (
             if(data.race[message.guild.id]) data.race[message.guild.id].join(message)
             else {
                 if(isNaN(message.data.arg[1]) || !Number.isInteger(Number(message.data.arg[1]))) return message.reply('진행중인 게임이 존재하지 않습니다.\n`{prefix}경주 참가 [배팅금]`으로 새로운 게임을 만들어보세요!'.bind({ prefix: message.data.prefix }))
-                if(message.data.arg[1] < 70 && message.data.arg[1] != 0) return message.reply('70원 이상 또는 0원의 배팅금을 설정해야합니다!')
+                if(message.data.arg[1] < 70 && Number(message.data.arg[1]) !== 0) return message.reply('70원 이상 또는 0원의 배팅금을 설정해야합니다!')
                 const race = new Race(message, knex)
                 if(!race) return message.reply('문제가 발생하였습니다. 다시 시도해주세요.')
                 else {
