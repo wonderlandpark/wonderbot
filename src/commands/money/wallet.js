6df2476b23caf4dd76e9f3a10038f6759aa45a51
Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>
2020-08-13 00:25:22 +0900

    fix: typo

--- src/commands/money/wallet.js
+++ src/commands/money/wallet.js
@@ -37,7 +37,7 @@ module.exports.execute = async (
         locale.commands.wallet.item,
         items.length === 0 ? locale.commands.wallet.noitem : items.replace('\n', '')
     )
-    embed.addField('빛', locale.commands.wallet.money.bind({ money: (money + Number(user.loan_money)).formatIt() }))
+    embed.addField('빚', locale.commands.wallet.money.bind({ money: (money + Number(user.loan_money)).formatIt() }))
     message.channel.send(embed)
 }
 
