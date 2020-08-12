28e735a349933d6b5205af92aa845c94893a8eb1
Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>
2020-08-13 00:14:36 +0900

    fix: invalid usage of forEach

--- src/commands/dev/sendmail.js
+++ src/commands/dev/sendmail.js
@@ -9,14 +9,13 @@ module.exports.execute = async (
     if(!message.data.arg[1]) return message.reply(locale.error.usage(message.data.cmd, message.data.prefix))
     message.reply(message.data.arg2)
     const users = await knex('users')
-    await users.forEach(async el=> {
+    for (const user of users) {
         let mail = JSON.parse(el.mails)
         let cooldown = JSON.parse(el.cooldown)
         mail.push({ read: false, date: Number(new Date()), content: message.data.arg2, send: message.data.arg[0]})
         cooldown.mail = 0
         await knex('users').update({ cooldown: JSON.stringify(cooldown), mails: JSON.stringify(mail)}).where({ id: el.id })
-    })
-    
+    }    
     await message.reply('모든 유저에게 메일이 전송되었습니다.')
 }
 
