e1512adb79a652fb2a9a4f97be8307ec3d01efd2
원더 <wonderbotsupport@naver.com>
2020-08-14 15:53:27 +0900

    fix: bug

--- src/commands/money/bank.js
+++ src/commands/money/bank.js
@@ -29,7 +29,7 @@ module.exports.execute = async (
     } else if (message.data.arg[0] === '대출') {
         if((u.loan_date+259200) > new Date()/1000) return message.reply('현재 대출금에 대한 상환 시간이 남아있습니다. 상환 기간이 지난 다음에 추가대출이 가능합니다.')
         if(!message.data.arg[1]) return message.reply(`자산기준, **${avail}**원까지 대출하실 수 있습니다.\n\`${message.data.prefix}은행 대출 [금액]\`으로 은행에서 대출신청을 하실 수 있습니다.\n\`${message.data.prefix}은행 대출 최대\`로 대출 최대 한도로 대출하실 수 있습니다.`)
-        const reqMoney = message.data.arg[1] === '최대' ? avail : Number(message.data.arg[1])
+        const reqMoney = Math.round(message.data.arg[1] === '최대' ? avail : Number(message.data.arg[1]))
         if(isNaN(reqMoney) || !Number.isInteger(reqMoney)) return message.reply('올바르지 않은 금액입니다. 올바른 정수를 입력해주세요.')
         if(reqMoney > avail) return message.reply('대출 최대 한도 이상의 금액을 대출하실 수 없습니다.')
         if(reqMoney < 100) return message.reply('100원 이상만 대출금을 신청해주세요!')
@@ -37,14 +37,14 @@ module.exports.execute = async (
         embed.setDescription(`**${reqMoney}**원을 대출합니다. 계속하시겠습니까?\n\`\`\`md\n- 12시간 후부터 대출금 상환이 가능합니다.\n- 3일 이내에 상환하지 않으시면 신용등급이 하락합니다.\n- 이자율은 ${u.loan_lvl*10}%로, 이후 ${Math.round(reqMoney*(1+u.loan_lvl*0.1))}원을 상환하셔야합니다.\`\`\`\n계속하시려면 ✅ 로 반응하세요.`)
         const m = await message.reply(embed)
         m.react('✅')
-        await knex('users').update({ action: 1}).where({ id: message.author.id })
+        await knex('users').update({ action:  1}).where({ id: message.author.id })
         m.awaitReactions((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, { max: 1, time: 10000, error: ['time']}).then(async collected => {
             if(collected.size === 0) {
                 await knex('users').update({ action: 0 }).where({ id: message.author.id })
                 return message.reply('대출이 취소되었습니다. 다음번에도 편하게 상담주세요!')
             }
             const time = Math.round(Number(new Date()/1000))
-            await knex('users').where({ id: message.author.id }).update({ money: +u.money + Number(reqMoney), loan_money: u.loan_money + reqMoney*(1+u.loan_lvl*0.1), loan_date: time, action: 0 })
+            await knex('users').where({ id: message.author.id }).update({ money: Math.round(+u.money + Number(reqMoney)), loan_money: Math.round(u.loan_money + reqMoney*(1+u.loan_lvl*0.1)), loan_date: time, action: 0 })
             return message.reply(`원더은행을 이용해주셔서 감사합니다.\n고객님의 잔고에 **${reqMoney}**원이 추가되었으며, 12시간 후부터 대출금을 상환하실 수 있으며, **${new Date((time+259200)*1000).format('ko')}**까지 원금과 이자를 포함한 **${reqMoney*(1+u.loan_lvl*0.1)}**원을 상환하셔야합니다.\n현재 남은 빚은 **${u.loan_money + reqMoney*(1+u.loan_lvl*0.1)}**원 입니다.`)
             
         })
@@ -56,7 +56,7 @@ module.exports.execute = async (
         if(u.loan_money === 0) return message.reply('상환할 대출금이 없습니다.')
         if(!message.data.arg[1]) return message.reply(`**${u.loan_money}**의 빚이 있습니다.\n**${new Date((u.loan_date+43200)*1000).format('ko')}**(${new Date((u.loan_date+43200)*1000).fromNow('ko')})까지 상환 하셔야합니다.\n\`${message.data.prefix}은행 상환 [금액]\`으로 금액만큼 상환을 하실 수 있습니다.\n\`${message.data.prefix}은행 상환 전부\`로 빚의 전부를 상환하실 수 있습니다.`)
         if((u.loan_date+43200) > new Date()/1000) return message.reply(`상환은 대출 후 12시간부터 가능합니다.\n**${new Date((u.loan_date+43200)*1000).fromNow('ko')}**(${new Date((u.loan_date+43200)*1000).format('ko')})에 다시 시도해주세요.`)
-        const reqMoney = message.data.arg[1] === '전부' ? u.loan_money : Number(message.data.arg[1])
+        const reqMoney = Math.round(message.data.arg[1] === '전부' ? u.loan_money : Number(message.data.arg[1]))
         if(isNaN(reqMoney) || !Number.isInteger(reqMoney)) return message.reply('올바르지 않은 금액입니다. 올바른 정수를 입력해주세요.')
         if(reqMoney > u.loan_money) return message.reply('빚을 초과하는 금액을 상환하실 수 없습니다.')
         if(reqMoney > u.money) return message.reply('잔고가 부족합니다.')
