8aabdb0833c899ea3aa1e1abd488a572839dffc0
원더 <wonderbotsupport@naver.com>
2020-08-14 15:38:02 +0900

    feat: added bank

--- src/commands/money/bank.js
+++ src/commands/money/bank.js
@@ -1,5 +1,5 @@
 const available = [ 70000, 30000, 10000, 1500, 500]
-const tier = [ '다이아몬드', '플레티넘', '골드', '실버', '브론즈' ]
+const tier = [ 'VIP', '플레티넘', '골드', '실버', '브론즈' ]
 module.exports.execute = async (
     client,
     message,
@@ -8,6 +8,7 @@ module.exports.execute = async (
     tools,
     knex
 ) => {
+    if(message.guild.id !== '542599372836438016' && message.channel.id !== '743718965934686271') return message.reply('해당 기능은 베타 테스트중인 기능입니다. 사용하실 수 없습니다.')
     const u = (await knex('users').where({ id: message.author.id }))[0]
     const stocks = await knex('stocks')
     let userMoney = 0
@@ -21,8 +22,8 @@ module.exports.execute = async (
         embed.setTitle('🏦 은행 정보')
         if((u.loan_date+259200) > new Date()/1000) embed.setDescription(`${message.author}님의 상환 예정 금액은 **${u.loan_money}**원이며, ${new Date((u.loan_date+259200)*1000).format('ko')}(${new Date((u.loan_date+259200)*1000).fromNow('ko')})까지 상환하셔야합니다.\n\n⚠️ 대출금과 이자를 상환하지 않으시면, 신용등급이 1등급 하락하게 됩니다.`)
         else {
-            embed.setDescription(u.loan_money === 0 ? `${message.author}님의 자산기준 대출 가능 금액은 **${avail}**원이며, 현재 신용등급은 **${tier[u.loan_lvl-1]}**(${u.loan_lvl}등급)입니다.\n\`\`\`md\n- 3일이내에 모든 대출금과 이자를 상환하셔야합니다.\n- 이자율은 ${u.loan_lvl*10}%입니다.\n- 대출금을 상환하지 않으신다면 시즌초기화와 함께 강제로 보유 아이템을 압류하게됩니다.\n- 이전 시즌의 금융활동은 다음 시즌의 신용등급 평가에 영향을 줍니다.\`\`\`\n\`${message.data.prefix}은행 대출 [금액]\`으로 은행에서 대출신청을 하실 수 있습니다.\n\`${message.data.prefix}은행 대출 최대\`로 대출 최대 한도로 대출하실 수 있습니다.`
-                : `${message.author}님의 자산과 상환하지 않은 대출금 기준 대출 가능 금액은 **${avail}**원이며, 현재 신용등급은 **${tier[u.loan_lvl-1]}**(${u.loan_lvl}등급)입니다.\n\`\`\`md\n- 3일이내에 모든 추가 대출금과 이자를 상환하셔야합니다.\n- 이자율은 ${u.loan_lvl*10}%입니다.\n- 대출금을 상환하지 않으신다면 시즌초기화와 함께 강제로 보유 아이템을 압류하게됩니다.\n- 이전 시즌의 금융활동은 다음 시즌의 신용등급 평가에 영향을 줍니다.\`\`\`\n\`${message.data.prefix}은행 대출 [금액]\`으로 은행에서 대출신청을 하실 수 있습니다.\n\`${message.data.prefix}은행 대출 최대\`로 대출 최대 한도로 대출하실 수 있습니다.`)
+            embed.setDescription(u.loan_money === 0 ? `${message.author}님의 자산기준 대출 가능 금액은 **${avail}**원이며, 현재 신용등급은 **${tier[u.loan_lvl-1]}**(${u.loan_lvl}등급)입니다.\n\`\`\`md\n- 3일이내에 모든 대출금과 이자를 상환하셔야합니다. (3일마다 신용등급이 1등급 하락됩니다.)\n- 이자율은 ${u.loan_lvl*10}%입니다.\n- 대출금을 상환하지 않으신다면 시즌초기화와 함께 강제로 보유 아이템을 압류하게됩니다.\n- 이전 시즌의 금융활동은 다음 시즌의 신용등급 평가에 영향을 줍니다.\`\`\`\n\`${message.data.prefix}은행 대출 [금액]\`으로 은행에서 대출신청을 하실 수 있습니다.\n\`${message.data.prefix}은행 대출 최대\`로 대출 최대 한도로 대출하실 수 있습니다.`
+                : `${message.author}님의 자산과 상환하지 않은 대출금 기준 대출 가능 금액은 **${avail}**원이고 빚은 **${u.loan_money}**원이며, 현재 신용등급은 **${tier[u.loan_lvl-1]}**(${u.loan_lvl}등급)입니다.\n\`\`\`md\n- 3일이내에 모든 추가 대출금과 이자를 상환하셔야합니다.\n- 이자율은 ${u.loan_lvl*10}%입니다.\n- 대출금을 상환하지 않으신다면 시즌초기화와 함께 강제로 보유 아이템을 압류하게됩니다.\n- 이전 시즌의 금융활동은 다음 시즌의 신용등급 평가에 영향을 줍니다.\`\`\`\n\`${message.data.prefix}은행 대출 [금액]\`으로 은행에서 대출신청을 하실 수 있습니다.\n\`${message.data.prefix}은행 대출 최대\`로 대출 최대 한도로 대출하실 수 있습니다.`)
         }
         return message.reply(embed)
     } else if (message.data.arg[0] === '대출') {
@@ -33,7 +34,7 @@ module.exports.execute = async (
         if(reqMoney > avail) return message.reply('대출 최대 한도 이상의 금액을 대출하실 수 없습니다.')
         if(reqMoney < 100) return message.reply('100원 이상만 대출금을 신청해주세요!')
         embed.setTitle('🏦 대출 신청')
-        embed.setDescription(`**${reqMoney}**원을 대출합니다. 계속하시겠습니까?\n\`\`\`md\n-12시간 후부터 대출금 상환이 가능합니다.\n- 3일 이내에 상환하지 않으시면 신용등급이 하락합니다.\n- 이자율은 ${u.loan_lvl*10}%로, 이후 ${Math.round(reqMoney*(1+u.loan_lvl*0.1))}원을 상환하셔야합니다.\`\`\`\n계속하시려면 ✅ 로 반응하세요.`)
+        embed.setDescription(`**${reqMoney}**원을 대출합니다. 계속하시겠습니까?\n\`\`\`md\n- 12시간 후부터 대출금 상환이 가능합니다.\n- 3일 이내에 상환하지 않으시면 신용등급이 하락합니다.\n- 이자율은 ${u.loan_lvl*10}%로, 이후 ${Math.round(reqMoney*(1+u.loan_lvl*0.1))}원을 상환하셔야합니다.\`\`\`\n계속하시려면 ✅ 로 반응하세요.`)
         const m = await message.reply(embed)
         m.react('✅')
         await knex('users').update({ action: 1}).where({ id: message.author.id })
@@ -42,10 +43,9 @@ module.exports.execute = async (
                 await knex('users').update({ action: 0 }).where({ id: message.author.id })
                 return message.reply('대출이 취소되었습니다. 다음번에도 편하게 상담주세요!')
             }
-            const time = Number(new Date()/1000)
-            await knex('users').where({ id: message.author.id }).update({ money: u.money + Number(reqMoney), loan_money: u.loan_money + reqMoney*(1+u.loan_lvl*0.1), loan_date: time })
-            await knex('users').update({ action: 0 }).where({ id: message.author.id })
-            return message.reply(`원더은행을 이용해주셔서 감사합니다.\n고객님의 잔고에 **${reqMoney}**원이 추가되었으며, 12시간 후부터 대출금을 상환하실 수 있으며, **${new Date((time+259200)*1000).format('ko')}**까지 원금과 이자를 포함한 **${reqMoney*(1+u.loan_lvl*0.1)}**원을 상환하셔야합니다.\n현재 남은 빛은 **${u.loan_money + reqMoney*(1+u.loan_lvl*0.1)}**원 입니다.`)
+            const time = Math.round(Number(new Date()/1000))
+            await knex('users').where({ id: message.author.id }).update({ money: +u.money + Number(reqMoney), loan_money: u.loan_money + reqMoney*(1+u.loan_lvl*0.1), loan_date: time, action: 0 })
+            return message.reply(`원더은행을 이용해주셔서 감사합니다.\n고객님의 잔고에 **${reqMoney}**원이 추가되었으며, 12시간 후부터 대출금을 상환하실 수 있으며, **${new Date((time+259200)*1000).format('ko')}**까지 원금과 이자를 포함한 **${reqMoney*(1+u.loan_lvl*0.1)}**원을 상환하셔야합니다.\n현재 남은 빚은 **${u.loan_money + reqMoney*(1+u.loan_lvl*0.1)}**원 입니다.`)
             
         })
             .catch(async()=>{
@@ -54,9 +54,36 @@ module.exports.execute = async (
             })
     } else if(message.data.arg[0] === '상환') {
         if(u.loan_money === 0) return message.reply('상환할 대출금이 없습니다.')
+        if(!message.data.arg[1]) return message.reply(`**${u.loan_money}**의 빚이 있습니다.\n**${new Date((u.loan_date+43200)*1000).format('ko')}**(${new Date((u.loan_date+43200)*1000).fromNow('ko')})까지 상환 하셔야합니다.\n\`${message.data.prefix}은행 상환 [금액]\`으로 금액만큼 상환을 하실 수 있습니다.\n\`${message.data.prefix}은행 상환 전부\`로 빚의 전부를 상환하실 수 있습니다.`)
         if((u.loan_date+43200) > new Date()/1000) return message.reply(`상환은 대출 후 12시간부터 가능합니다.\n**${new Date((u.loan_date+43200)*1000).fromNow('ko')}**(${new Date((u.loan_date+43200)*1000).format('ko')})에 다시 시도해주세요.`)
+        const reqMoney = message.data.arg[1] === '전부' ? u.loan_money : Number(message.data.arg[1])
+        if(isNaN(reqMoney) || !Number.isInteger(reqMoney)) return message.reply('올바르지 않은 금액입니다. 올바른 정수를 입력해주세요.')
+        if(reqMoney > u.loan_money) return message.reply('빚을 초과하는 금액을 상환하실 수 없습니다.')
+        if(reqMoney > u.money) return message.reply('잔고가 부족합니다.')
+        embed.setTitle('🏦 대출금 상환')
+        embed.setDescription(`**${u.loan_money}**원의 빚중 **${reqMoney}**원을 상환합니다. 계속하시겠습니까?\n\n상환하시게되면 **${u.loan_money - reqMoney}**원의 빚이 남게됩니다.\n계속하시려면 ✅ 로 반응하세요.`)
+        const m = await message.reply(embed)
+        m.react('✅')
+        await knex('users').update({ action: 1}).where({ id: message.author.id })
+        m.awaitReactions((reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id, { max: 1, time: 10000, error: ['time']}).then(async collected => {
+            if(collected.size === 0) {
+                await knex('users').update({ action: 0 }).where({ id: message.author.id })
+                return message.reply('상환이 취소되었습니다.')
+            }
+            const point = u.loan_point + reqMoney
+            const loan_lvl = point >= available.map((r, n)=> r*(1+0.1*(n+1)))[u.loan_lvl-1] ? u.loan_lvl-1 : u.loan_lvl
+            await knex('users').where({ id: message.author.id }).update({ money: +u.money - Number(reqMoney), loan_money: u.loan_money - reqMoney, action: 0, loan_lvl, loan_point: loan_lvl !== u.loan_lvl ? 0 : point, loan_date: 0 })
+            message.reply(`원더은행을 이용해주셔서 감사합니다.\n**${u.loan_money}**원의 빚중에 **${reqMoney}**원을 상환하셨습니다.\n\n${u.loan_money - reqMoney > 0 ? `**${u.loan_money - reqMoney}**원의 빚을 **${new Date((u.loan_date+43200)*1000).format('ko')}**(${new Date((u.loan_date+43200)*1000).fromNow('ko')})까지 상환 하셔야합니다.` : '모든 빚을 상환하셨습니다!'}`)
+            if(loan_lvl !== u.loan_lvl) return message.reply(`축하드립니다. 신용등급이 상향되었습니다.\n**${tier[u.loan_lvl-1]}**(${u.loan_lvl}등급) -> **${tier[loan_lvl-1]}**(${loan_lvl}등급)\n\n더 많은 금액을 대출하실 수 있으며, 다양한 혜택을 누려보세요.`)
+
+            
+        })
+            .catch(async()=>{
+                await knex('users').update({ action: 0 }).where({ id: message.author.id })
+                return message.reply('상환이 취소되었습니다.')
+            })
+
         
-        // const reqMoney = message.data.arg[1] === '전부' ? avail : Number(message.data.arg[1])
 
     }
     
