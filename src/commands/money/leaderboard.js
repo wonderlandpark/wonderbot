845ab3f8d3bdaf06e8e0cbaa07a3929a77d49fd6
원더 <wonderbotsupport@naver.com>
2020-09-06 11:44:16 +0900

    feat: caching leaderboard

--- src/commands/money/leaderboard.js
+++ src/commands/money/leaderboard.js
@@ -4,7 +4,9 @@ module.exports.execute = async (
     locale,
     embed,
     tools,
-    knex
+    knex,
+    props,
+    data
 ) => {
     const stocks = await knex('stocks').select('*')
 
@@ -13,36 +15,38 @@ module.exports.execute = async (
     } else {
         message.guild.members.fetch()
         var leaderboard =
-           ['전체', '전', 'ㅈ', 'global'].includes( message.data.arg[0])
+           ['전체', '전', 'ㅈ', 'global'].includes(message.data.arg[0])
                ? await knex.select('*').from('users')
                : (await knex.select('*').from('users')).filter(r =>
                    message.guild.members.cache.get(r.id)
                )
-        var txt = ''
-        leaderboard.sort(function (a, b) {
-            var bm = 0
-            var am = 0
-            Object.keys(JSON.parse(b.items)).forEach(el => {
-                bm += stocks.find(i => i.name === el).now * JSON.parse(b.items)[el]
+        let server = ''
+        if(new Date() - data.leaderboard.updated > 60000) {
+            let txt = ''
+            leaderboard.sort(function (a, b) {
+                var bm = 0
+                var am = 0
+                Object.keys(JSON.parse(b.items)).forEach(el => {
+                    bm += stocks.find(i => i.name === el).now * JSON.parse(b.items)[el]
+                })
+                Object.keys(JSON.parse(a.items)).forEach(el => {
+                    am += stocks.find(i => i.name === el).now * JSON.parse(a.items)[el]
+                })
+                if (Number.isNaN(am)) am = 0
+                if (Number.isNaN(bm)) bm = 0
+                return bm + Number(b.money) - (am + Number(a.money))
             })
-            Object.keys(JSON.parse(a.items)).forEach(el => {
-                am += stocks.find(i => i.name === el).now * JSON.parse(a.items)[el]
-            })
-            if (Number.isNaN(am)) am = 0
-            if (Number.isNaN(bm)) bm = 0
-            return bm + Number(b.money) - (am + Number(a.money))
-        })
 
-        for (var i = 1; i < 16; i++) {
-            var m = 0
-            if (leaderboard[i - 1]) {
-                Object.keys(JSON.parse(leaderboard[i - 1].items)).forEach(el => {
-                    m +=
+            for (var i = 1; i < 16; i++) {
+                let m = 0
+                if (leaderboard[i - 1]) {
+                    Object.keys(JSON.parse(leaderboard[i - 1].items)).forEach(el => {
+                        m +=
                         stocks.find(i => i.name === el).now *
                         JSON.parse(leaderboard[i - 1].items)[el]
-                })
-                let userData = await client.users.fetch(leaderboard[i - 1].id, false)
-                txt +=
+                    })
+                    let userData = await client.users.fetch(leaderboard[i - 1].id, false)
+                    txt +=
                     `\n${i}. [${
                         userData
                             ? userData.username +
@@ -52,8 +56,12 @@ module.exports.execute = async (
                     }](${locale.commands.leaderboard.all} ` +
                     (numberToKorean(m + Number(leaderboard[i - 1].money))||0) + locale.commands.money.won + (leaderboard[i - 1].loan_money !== 0 ? ' - 빚 ' + numberToKorean(leaderboard[i - 1].loan_money) + locale.commands.money.won : '')
                     + ')'
+                }
             }
+            if(['전체', '전', 'ㅈ', 'global'].includes(message.data.arg[0])) data.leaderboard = { updated: new Date(), txt }
+            else server = txt
         }
+        
         message.channel.send(
             '```md\n' +
             locale.commands.leaderboard.leaderboard.bind({
@@ -66,7 +74,7 @@ module.exports.execute = async (
                         server: message.guild.name
                     })
             }\n ` +
-            txt +
+            (server || data.leaderboard.txt) +
             '```'
         )
     }
